25e8e4c30847892b7a4f124de6b436407d74b290
Mikhail Cruz Andrade <jailandrade@gmail.com>
2019-07-09 19:15:14 -0500

    order by

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RequestsService } from '../requests.service';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-characters',
@@ -12,16 +14,27 @@ export class CharactersComponent implements OnInit {
   character = {};
 
   constructor(
-    private requests: RequestsService
+    private requests: RequestsService,
+    private route: ActivatedRoute,
   ) {}
 
   ngOnInit() {
-    this.getCharacters();
+
+
+    this.route.queryParams.subscribe(params => {
+      console.log(params)
+      if (params.ordenar) {
+        let query = params.ordenar;
+
+        this.getCharacters(query);
+      } else {
+        this.getCharacters('');
+      }
+    })
+
   }
 
   getCharacterByName(event) {
-
-    console.log(event.target.value);
 
     this.requests.getCharacterByName(event.target.value)
       .subscribe((res) => {
@@ -35,11 +48,54 @@ export class CharactersComponent implements OnInit {
       });
   }
 
-  getCharacters() {
+  getCharacters(type: string) {
     this.requests.getCharacters()
       .subscribe((res) => {
-        this.characters = res['results'];
+         this.characters = res['results'].map((c) => {
+          let character = {};
+
+          character['nombre'] = c.name;
+          character['altura'] = c.height;
+          character['peso'] = c.mass;
+
+          return character;
+        })
+
+        if (type != '' && type === 'nombre') {
+          this.orderByName(type);
+        } else if (type != '' && type !== 'nombre') {
+          this.orderByNumber(type);
+        }
+
       });
+  }
+
+  orderByName(type) {
+    console.log('ordering by nombre');
+    this.characters.sort(function (a, b) {
+      if (a[type] > b[type]) {
+        return 1;
+      }
+      if (a[type] < b[type]) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    });
+  }
+
+  orderByNumber(type) {
+    console.log('ordering by ', type);
+    this.characters.sort(function (a, b) {
+      if (parseInt(a[type]) > parseInt(b[type])) {
+        return 1;
+      }
+      if (parseInt(a[type]) < parseInt(b[type])) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    });
   }
 
 }
